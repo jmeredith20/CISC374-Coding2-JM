fddadbe19c07a87a465ac7e2c6d723bbb32bc382
Jacob Meredith <79116619+jmeredith20@users.noreply.github.com>
2024-02-25 01:57:35 -0500

    Completed Part 1 (Officially this time)

--- src/scenes/mainScene.ts
+++ src/scenes/mainScene.ts
@@ -1,12 +1,27 @@
 import Phaser from "phaser";
 
 export default class MainScene extends Phaser.Scene {
+    private platforms?: Phaser.Physics.Arcade.StaticGroup;
+
     constructor() {
         super({ key: "MainScene" });
     }
 
     create() {
         this.add.image(400, 300, "sky");
+        this.platforms = this.physics.add.staticGroup();
+
+        const ground = this.platforms.create(
+            400,
+            540,
+            "ground"
+        ) as Phaser.Physics.Arcade.Sprite;
+
+        ground.setScale(2).refreshBody();
+
+        this.platforms.create(600, 400, "ground");
+        this.platforms.create(50, 250, "ground");
+        this.platforms.create(750, 220, "ground");
 
         const message = `Phaser v${Phaser.VERSION}`;
         this.add
--- src/scenes/preloadScene.ts
+++ src/scenes/preloadScene.ts
@@ -9,7 +9,7 @@ export default class PreloadScene extends Phaser.Scene {
         this.load.image("sky", "assets/sky.png");
         this.load.image("star", "assets/star.png");
         this.load.image("bomb", "assets/bomb.png");
-        this.load.image("ground", "assets/ground.png");
+        this.load.image("ground", "assets/platform.png");
         this.load.spritesheet("dude", "assets/dude.png", {
             frameWidth: 32,
             frameHeight: 48,
